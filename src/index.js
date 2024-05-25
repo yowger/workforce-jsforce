458ab43e23e045a47f8e78a9b9a74b503c17a5e6
Roger Pantil <rogerpantilyowger@gmail.com>
2024-05-25 15:49:24 +0800

    modified login

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ var dotenv = require("dotenv")
 
 dotenv.config({ path: ".env" })
 
+const LOGIN_URL = process.env.JSFORCE_LOGIN_URL
 const conn = new jsforce.Connection({
-    loginUrl: "https://test.salesforce.com",
+    loginUrl: LOGIN_URL,
 })
 
-async function jsForceLogin(username, password) {
+async function jsForceLogin(username, password, token) {
     try {
-        const userInfo = await conn.login(username, password)
+        const userInfo = await conn.login(username, password + token)
         return userInfo
     } catch (error) {
         console.error("JsForce Login failed: " + error)
@@ -18,10 +19,11 @@ async function jsForceLogin(username, password) {
 }
 
 async function main() {
-    const username = process.env.JSFORCE_USERNAME
-    const password = process.env.JSFORCE_PASSWORD
+    const USERNAME = process.env.JSFORCE_USERNAME
+    const PASSWORD = process.env.JSFORCE_PASSWORD
+    const TOKEN = process.env.JSFORCE_SECURITY_TOKEN
 
-    const userInfo = await jsForceLogin(username, password)
+    const userInfo = await jsForceLogin(USERNAME, PASSWORD, TOKEN)
     console.log("user data: " + userInfo)
 }
 
